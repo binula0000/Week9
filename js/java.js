b4db5fb2729ddb82cd1ffeb3c3f70946d9d0eafa
binula0000 <binulakaiynbai@gmail.com>
2024-10-31 14:13:30 +0500

    First commit

---
+++ js/java.js
@@ -0,0 +1,16 @@
+let name = "Binula";
+let age = 21;
+let isStudent = true;
+
+console.log("Аты: " + name + " | Тип: " + typeof name);
+console.log("Жасы: " + age + " | Тип: " + typeof age);
+console.log("Студент пе? " + isStudent + " | Тип: " + typeof  isStudent);
+
+
+age = age.toString();
+console.log("Жасы (жол ретинде): " + age + " | Тип: " + typeof  age);
+
+
+ 
+
+
